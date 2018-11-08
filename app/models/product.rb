5321330be88335f1de4326aaf11d0e79945d22ea
vanstoker <vanstoker@mail.ru>
2018-11-08 18:08:49 +0300

    I'm back

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ApplicationRecord
+	has_many :line_items
+
+	before_destroy :ensure_not_referenced_by_any_kine_item
 
 	validates :title, :description, :image_url, presence: true
 	validates :price, numericality: {greater_than_or_equal_to: 0.01}
@@ -7,4 +10,15 @@ class Product < ApplicationRecord
 		with: %r{\.(gif|jpg|png)}i,
 		message: 'URL must be in format GIF, JPG or PNG.'
 	}
+
+	private
+
+	def ensure_not_referenced_by_any_line_item
+		if line_items.empty?
+			return true
+		else
+			errors.add(:base, "have the products items")
+			return false
+		end
+	end
 end
