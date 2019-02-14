57f4929671c51857055cafe5f261a1d66e92992c
vanstoker <vanstoker@mail.ru>
2019-02-14 23:10:47 +0300

    Fucking jquery-ui! How are you working?

--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -17,7 +17,7 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
 
   test "should create line_item" do
     assert_difference('LineItem.count') do
-      post :create, product_id: products(:ruby.id
+      post :create, product_id: products(:ruby.id)
     end
     assert_redirected_to cart_path(assigns(:line_item).cart)
   end
